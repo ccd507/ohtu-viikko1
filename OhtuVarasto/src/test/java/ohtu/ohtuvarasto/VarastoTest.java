b6472d4b1b98b7be9d416ca1a43552017ecfbdb2
ccd507 <ccd507c45565b2cd3abcc185f19ebf@gmail.com>
2015-03-15 11:45:06 +0200

    Revert "Make one test to fail again"
    This reverts commit 175e246fcacfa8fad44d9b2feb05c8983af5ffb5.

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -88,7 +88,7 @@ public class VarastoTest {
     public void otetaanNegatiivinenMäärä() {
         double tila = varasto.paljonkoMahtuu();
         varasto.otaVarastosta(-1);
-        assertEquals(999, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(tila, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
     @Test
